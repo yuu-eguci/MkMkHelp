029f973d1a178293385269608b36bf89cfcc6a8c
yuu-eguci <yuu.eguci@gmail.com>
2025-03-24 15:56:39 +0900

    ✨ location

--- README.md
+++ README.md
@@ -14,5 +14,5 @@ pipenv sync
 # mkmk_help total_row を編集。
 
 # 実行。本物の URL の掲載を避けています。 URL は適切に変えること。
-pipenv run python mkmk_help.py --base-url https://WWW.JAV.OR.JP
+pipenv run python mkmk_help.py --base-url https://WWW.JAV.OR.JP --total-row 1
 ```
--- mkmk_help.py
+++ mkmk_help.py
@@ -19,29 +19,39 @@ def main() -> None:
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--base-url", default="", help="Base url")
+    parser.add_argument("--total-row", type=int, default=1, help="Number of rows to fetch")
     args = parser.parse_args()
     base_url = args.base_url
-
     # メッチャおもろいんだけど一度の表示件数をコッチで決めれるｗ
-    total_row = 5
+    total_row = args.total_row
+
     list_url = f"{base_url}/compatible_organizations?page=1&standards=140012015&pageSize={total_row}&nationality=JPN&classification=28"  # noqa: E501
 
     # html ゲット。
     html: str = fetch_html_slowly(list_url, wait_sec=3)
 
     # 会社名と、リンクをゲット。
+    # [{name, path}, ...]
     organizations: list[dict[str, str]] = extract_organizations_from_html(html)
+    for org in organizations:
+        # [{name, url}, ...]
+        org["url"] = f"{base_url}{org['path']}"
+        del org["path"]
 
     for i, org in enumerate(organizations):
         # 1社ずつ詳細画面にアクセス。
-        detail_url = f"{base_url}{org['path']}"
-        # print(detail_url)
-        detail_html: str = fetch_html(detail_url)
+        detail_html: str = fetch_html(org["url"])
         # print(detail_html)
         detail_json: dict = extract_next_data_from_html(detail_html)
-        # print(detail_json)
-        org["regis_num"] = detail_json["props"]["pageProps"]["organization"]["attributes"]["regis_num"]
-        del org["path"]
+        # print(detail_json) # <-- json みたいならこれ
+        # NOTE: 順番を変えたいだけ。
+        # [{name, location, url}, ...]
+        org = {
+            "name": org["name"],
+            "location": detail_json["props"]["pageProps"]["organization"]["attributes"]["location"],
+            "url": org["url"],
+        }
+        organizations[i] = org
         show_progress_with_name(i + 1, len(organizations), org["name"])
         # NOTE: 連続アクセスをやめようか。
         sleep(0.5)
