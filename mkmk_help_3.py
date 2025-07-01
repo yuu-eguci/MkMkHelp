e960bee8310b08385227b72f412e090edfe71344
yuu-eguci <yuu.eguci@gmail.com>
2025-07-01 09:54:41 +0900

    ✨ Level 3 検索 URL を作成する

--- mkmk_help_3.py
+++ mkmk_help_3.py
@@ -30,6 +30,15 @@ def main() -> None:
     df = pd.read_csv(args.csv)
     logger.info(f"CSV 読み込みおｋ: {args.csv}")
 
+    # 検索ワード (次項で使う) を作成します。
+    # NOTE: 住所からスペースを除去して検索用に正規化
+    df["location_no_space"] = df["location"].apply(jn.remove_spaces)
+
+    # 検索 URL の列を追加 (ここでは location_no_space を使う)
+    df["jn_search_url"] = df["location_no_space"].apply(lambda x: jn.create_search_url(base_url, x))
+
+    logger.info("検索 URL の作成おｋ")
+
     # TODO: 以降の処理を実装予定
 
     logger.info("end mkmk_help_3")
